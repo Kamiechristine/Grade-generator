5f2dfcc0338836cf5e2e6f51a08e13343823b2ac
Kamiechristine <christinemaina5141@gmail.com>
2024-12-02 22:58:51 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-function studentMarks(marks){
-    let grade;
-    if(marks >79){
-         grade = 'A';
-    }else if(marks =>60 && marks <=79){
-         grade = 'B';
-    }else if(marks =>50 && marks<=59){
-         garde = 'C';
-    }else if(marks =>40 && marks<=49){
-        grade ='D';
-    }else{
-        grade ='E';
-    }
-    return grade;
-}
-console.log(studentMarks(72));
+//You have to get a function and also request user enter the students marks here.
+
+function scoreEvaluator() {
+
+    const points = ("Enter the student's marks (0 - 100):");
+  
+    const score = (points);
+  
+    //So here you have to give the range of the marks entered by the user not to exceed 100 and not to go below 0
+  
+    if (score < 0 || score > 100 || (!score)) {//The second or is for Not a number.
+  
+        console.log("Invalid input. Please enter a number between 0 and 100.");//If the s
+console.log("Invalid input. Please enter a number between 0 and 100.");//If the score is <0 or is >100,it notifies the user that the data entered is invalid and suggests that they enter valid data.
+  
+      } else if (score > 79) {//If the score is > than 79, it displays the grade "A"
+  
+        console.log("Grade: A");
+  
+      } else if (score >= 60) {//If the score is >=60, it displays the grade "B"
+  
+        console.log("Grade: B");
+  
+      } else if (score >= 49) {// If the score is >=49,t displays the grade "C"
+  
+        console.log("Grade: C");
+  
+      } else if (score >= 40) {// If the score is >=40, it displays the grade "D"
+  
+        console.log("Grade: D");
+  
+      } else {
+  
+        console.log("Grade: E");//If the score is 39 and below, it displays the grade "E"
+  
+      }
+  
+  }
+  scoreEvaluator(50)
